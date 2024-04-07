9f1ef2c93c19f4f0c97b3197d7b00f8e80aef085
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-04-07 22:43:54 +0530

    understanding requests

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
   //   console.log(res);
 });
 
