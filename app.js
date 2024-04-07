79b459f790b135e94c2bd3fc2f26b28f9aa5abff
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-04-07 22:53:51 +0530

    sending response

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//core modules of node.
-//http:- Hyper Text Transfer Protocol
-// https:- Hyper Text Transfer Protocol Secure
-// fs:- File System
-// path
-// os
-
-//creating our first node server
+//Sending Response
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   console.log(res);
+  //   console.log(req.url, req.method, req.headers);
+  res.setHeader("Context-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node.js</title></head>");
+  res.write(
+    "<body><h1>Hello this is created form our node.js server</h1></body>"
+  );
+  res.write("</html>");
+  res.end(); //after res.end() if we write res.write then node is gonna throw error
 });
 
 server.listen(2000);
